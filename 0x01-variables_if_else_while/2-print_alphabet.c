32222a0b8ee57ce701e6e61861e09e299b97a91e
Mosestack <lawalabiodun876@gmail.com>
2023-08-14 18:23:03 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - a program that prints lowercase alphabets
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'Z') {
+			putchar(c);
+				c++;
+		}
+	putchar('\n');
+	return (0);
+}
