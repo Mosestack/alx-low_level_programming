c2001eeeb1063234d0e79476b21580cae0a7a237
Mosestack <lawalabiodun876@gmail.com>
2023-08-16 17:32:36 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,23 +2,24 @@
 
 /**
  * print_alphabet_x10 - replicate alphabet *10times
- * Return:void
+ * Return: void
  */
 
 void print_alphabet_x10(void)
 {
-	char ch;
-	int i;
 
-	i = 0;
-	while (1 < 10)
-	{
-		for (ch = 'a'; ch <= 'z'; ch++)
-		{
-			_putchar(ch);
-		}
-		_putchar('\n');
-		i++;
-	}
+char ch;
+int i;
+i = 0;
+while (1 < 10)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+i++;
+}
+
 
 }
