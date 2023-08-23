5295dc04ad33b53f10ab2cdfc9485f709407ea1d
Mosestack <lawalabiodun876@gmail.com>
2023-08-22 20:32:03 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - a program that reset the value of
+ * an int to 98
+ *
+ * @n: the value of differencing pointer
+ * Return: nth
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
